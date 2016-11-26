29299972e946afaf2d700b1e232ea6d42ffa3523
zhongxuqi <zhongxuqi.1991@gmail.com>
2016-11-26 18:55:05 +0800

    update page overview

--- front/src/js/components/pages_overview.jsx
+++ front/src/js/components/pages_overview.jsx
@@ -3,22 +3,21 @@ import React from 'react'
 export default class PagesOverView extends React.Component {
     render() {
         return <div className={[this.props.class, "lowtea-page-overview"].join(" ")}>
-            <div className="navigation clearfix">
-                <ul className="pull-left">
-                    <li className="item active">
-                        <a href="">推荐排序</a>
-                    </li>
-
-                    <li className="item">
-                        <a href="">时间排序</a>
-                    </li>
-                </ul>
-                <div className="form-group pull-right lowtea-searchbar">
-                    <div className="input-group">
-                        <div className="input-group-addon"><span className="glyphicon glyphicon-search"></span></div>
-                        <input className="form-control" placeholder="请输入关键词"/>
-                    </div>
+            <div className="col-md-9 page-list-container" style={{padding:"0px"}}>
+                <div className="lowtea-searchbar clearfix">
+                    <div className="col-xs-11"><input className="form-control" placeholder="请输入关键词"/></div>
+                    <div className="col-xs-1"><button className="btn btn-default pull-right">搜索</button></div>
                 </div>
+                
+                <div style={{margin:"0px 20px"}}>
+                    <h4>一共找到了858篇文章</h4>
+
+                    <ul className="page-list">
+
+                    </ul>
+                </div>
+            </div>
+            <div className="col-md-3">
             </div>
         </div>
     }
