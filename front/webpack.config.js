a3706e80dc39dcb91f79634edf30a59376a86380
zhongxuqi <zhongxuqi.1991@gmail.com>
2016-12-24 14:07:46 +0800

    update webpack.config.js

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -24,11 +24,11 @@ module.exports = {
         ]
     },
     plugins: [
-        // new webpack.optimize.UglifyJsPlugin({
-        //     compress: {
-        //         warnings: false
-        //     }
-        // })
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
     ],
     devtool: 'source-map',
 }
