c7e9960e49c2d1081ae41741ea7d4e77269f13a8
zhongxuqi <zhongxuqi.1991@gmail.com>
2017-03-19 11:57:56 +0800

    update language

--- LowteaApp/app/config/net.js
+++ LowteaApp/app/config/net.js
@@ -7,8 +7,8 @@ function formatHeadImg(input) {
 }
 
 export default {
-    Host: "http://blog.electronlowtea.tech", 
-    // Host: "http://192.168.0.101:7070",
+    // Host: "http://blog.electronlowtea.tech", 
+    Host: "http://192.168.0.101:7070",
     // Host: "http://192.168.201.196:7070",
 
     FormatHeadImg: formatHeadImg,
