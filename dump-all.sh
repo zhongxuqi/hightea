1d486c614e7e3d5a8da4131107f2d450c6f5d16d
zhongxuqi <zhongxuqi.1991@gmail.com>
2017-03-20 12:59:10 +0800

    add dump shell

--- dump-all.sh
+++ dump-all.sh
@@ -1,6 +1,26 @@
 #/bin/bash
-
-if [ $# -lt 6 ]; then
+echo "begin..."
+if [ $# -ne 3 ]; then
     echo "error args: dump-all.sh <mongohost> <mongodb> <mediaPath>"
+    exit 1
 fi
 
+if [ ! -x `pwd`/dumps ]; then
+    echo "dumps not found"
+    mkdir dumps
+fi
+
+if [ -x `pwd`"/tmp" ]; then
+    rm -rf tmp
+    mkdir tmp
+else
+    mkdir tmp
+fi
+
+pushd tmp
+mongodump --host $1 -d $2 -o .
+tarname=`date +%Y-%m-%dT%H-%M-%S`".tar.gz"
+tar zcvf $tarname $2 $3
+mv $tarname ../dumps/
+popd
+echo "end..."
